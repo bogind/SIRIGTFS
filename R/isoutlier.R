b512e4e08d00002e40b2a42eaf4f9038ed51ebf2
bogind <bogind@post.bgu.ac.il>
2017-07-29 09:33:26 +0300

    made changes to is.outlier writing protocol, should work now.

--- R/isoutlier.R
+++ R/isoutlier.R
@@ -1,11 +1,15 @@
-is.outlier <- function(spSIRI, trip){
+is.outlier <- function(spSIRI, trip, outliers){
   require("rgeos", quietly = TRUE)
   siridfch <- rgeos::gConvexHull(spSIRI[spSIRI@data$trip_id == trip,])
   if(!is.null(siridfch)){
     cent1 <- rgeos::gCentroid(siridfch)
     centbuffer <- rgeos::gBuffer(cent1, width = 75)
     outlier <- rgeos::gWithin(spSIRI, centbuffer)
-    return(outlier)
+    if(outlier){
+      print(paste("trip_id ",trpi, " from lineref number" ,w, " is an outlier"))
+      print("moving on.")
+      outliers <<- c(outliers,paste(trip, " ,", i))
+    }
   }else{
     print(paste('failed trip: ', trip))
   }

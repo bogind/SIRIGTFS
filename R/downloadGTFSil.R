ba2dd37977342ab6afee9cc71a9f7fbb8ce902a4
Dror Bogin <Dror.Bogin@gmail.com>
2019-05-29 09:08:47 +0300

    fix download GTFSIL

--- R/downloadGTFSil.R
+++ R/downloadGTFSil.R
@@ -24,4 +24,4 @@ downloadGTFSil <- function(directory = NULL){
   date <- Sys.Date()
   date <- as.character(as.POSIXct(strptime(date, format = "%Y-%m-%d")))
   file_name <- as.character(paste("GTFS",date,".zip", sep = ""))
-  download.file("ftp://gtfs.mot.gov.il/israel-public-transportation.zip", destfile = paste0(directory,file_name), method = "libcurl")}
+  download.file("ftp://gtfs.mot.gov.il/israel-public-transportation.zip", destfile = paste0(directory,"/",file_name), method = "libcurl")}
